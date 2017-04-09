ed769e835b6bd4e38afbd4ca3805341272390563
jasonetco <jasonetco@gmail.com>
2017-04-09 15:29:26 -0400

    :rabbit: Optimize for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
+const browsers = [
+  'last 2 versions',
+  'ios_saf >= 8',
+  'ie >= 10',
+  'chrome >= 49',
+  'firefox >= 49',
+  '> 1%',
+];
+
 module.exports = {
-    entry: "./main.js",
+    entry: [
+        'babel-polyfill',
+        "./main.js",
+    ],
     output: {
         path: __dirname,
         filename: "dist/bundle.js"
     },
     module: {
-        loaders: [
-            { test: /\.json$/, loader: "json" }
-        ]
-    }
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['env', {
+                            targets: { browsers },
+                            debug: false,
+                            loose: true,
+                            modules: false,
+                            useBuiltIns: true,
+                        }],
+                    ],
+                },
+            },
+        }],
+    },
 };
